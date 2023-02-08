968b78644e8295a7873676b8ecd8ec9971fc84a3
ehachi <ehajianto@gmail.com>
2023-02-08 23:43:41 +0300

    Complet

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
-
-
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    
+    def initialize(title)
+    @title = title
+    @page_count = 0
+    end
+    
+    def turn_page
+    @page_count += 1
+    puts "Flipping the page...wow, you read fast!"
+    end
+    end
+    
+    class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    
+    def initialize(brand)
+    @brand = brand
+    @condition = "used"
+    end
+    
+    def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+    end
+    end
